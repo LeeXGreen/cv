f8f5b014d285978088b0a7c82138acbf17976008
Lee Green <lee.x.green@gmail.com>
2013-12-05 21:14:38 -0500

    allow switching ruby; now takes an argument

--- buildit.sh
+++ buildit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ruby1.9.3 biteydown/bin/biteydown --html --pdf -c style.css cv.md
+RUBY=ruby
+#RUBY=ruby1.9.3
+#ruby1.9.3 biteydown/bin/biteydown --html --pdf -c style.css cv.md
+$RUBY biteydown/bin/biteydown --html --pdf -c style.css $1
