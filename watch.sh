56855c51f9de3d769242f0da2b07b6052962e7ea
Lee Green <lee.x.green@gmail.com>
2014-05-06 15:18:48 -0500

    improve watch script so that CSS changes are picked up

--- watch.sh
+++ watch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #FILE="$1"
 #CMD="$2"
+WATCH_EXPR="style.css cv.md"
 FILE="cv.md"
 CMD="./buildit.sh"
 "$CMD" "$FILE" # run it first off
-LAST=`ls -l "$FILE"`
+LAST=`ls -l $WATCH_EXPR`
 while true; do
   sleep 1
-  NEW=`ls -l "$FILE"`
+  NEW=`ls -l $WATCH_EXPR`
   if [ "$NEW" != "$LAST" ]; then
     echo "running it"
     "$CMD" "$FILE"
